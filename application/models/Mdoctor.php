019d542fa6ac2c8ba9a4dc9546ffdd9be67ba845
Asanka De Silva <easankad@gmail.com>
2020-04-19 22:35:10 +0530

    doctor id = guid

--- application/models/Mdoctor.php
+++ application/models/Mdoctor.php
@@ -75,6 +75,7 @@ class Mdoctor extends CI_Model
 		$result = null;
 
 		$this->post['id'] = trim(com_create_guid(), '{}');
+		
 		$this->post['is_deleted'] = 0;
 		$this->post['is_active'] = 1;
 		$this->post['updated'] = date("Y-m-d h:i:s");
