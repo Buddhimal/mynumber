69bd3b6e8319bc625b68ccab7b22d227ab7f843b
Asanka De Silva <easankad@gmail.com>
2020-04-19 22:26:24 +0530

    Improved the validation and creation logics

--- application/controllers/api/Consultant.php
+++ application/controllers/api/Consultant.php
@@ -44,82 +44,37 @@ class Consultant extends REST_Controller
 
 			if ($check_auth_client == true) {
 
-				$doctor_id = trim(com_create_guid(), '{}');
-				$post = $this->input->post();
+				// Passing post array to the model.
+				$this->mdoctor->set_data( $this->input->post() );
 
-				/*
-				POSTs
+				// model it self will validate the input data
+				if( $this->mdoctor->is_valid() ){
 
-				salutation,
-				firstname,
-				lastname,
-				wellknownas,
-				contact_telephone,
-				contact_mobile,
-				device_mobile,
-				email,
-				specialities,
-				slmc_reg_number
-				consulting_hospitals
-				*/
+					// create the doctor record as the given data is valid
+					$doctor = $this->mdoctor->create();
 
-				/*
-				id:guid, salutation:string(5), first_name:text, last_name:text, nic:varchar(20), contact_telephone:varchar(15), contact_mobile, device_mobile, email:text,  known_name:text, location:guid, specialities:json, doctor_code:guid, is_deleted:bool, is_active:bool, updated:datetime, created:datetime, updated_by:guid, created_by:guid
-				*/
+					if(!is_null($doctor)) {
 
+						$response = new stdClass();
 
-				$doctor_data['id'] = $doctor_id;
-				$doctor_data['salutation'] = $this->input->post('salutation');
-				$doctor_data['first_name'] = $this->input->post('firstname');
-				$doctor_data['last_name'] = $this->input->post('lastname');
-				$doctor_data['known_name'] = $this->input->post('wellknownas');
-				$doctor_data['contact_telephone'] = $this->input->post('telephone');
-				$doctor_data['email'] = $this->input->post('email');
-				$doctor_data['specialities'] = $this->input->post('specialities');
-				$doctor_data['slmc_reg_number'] = $this->input->post('slmc_reg_number');
-				$doctor_data['consulting_hospitals'] = $this->input->post('consulting_hospitals');
-				$doctor_data['is_deleted'] = 0;
-				$doctor_data['is_active'] = 1;
-				$doctor_data['updated'] = date("Y-m-d h:i:s");
-				$doctor_data['created'] = date("Y-m-d h:i:s");
-				$doctor_data['updated_by'] = '';
-				$doctor_data['created_by'] = '';
+						$response->status = REST_Controller::HTTP_OK;
+						$response->msg = 'New Doctor Added Successfully';
+						$response->response = $doctor;
+						$this->response($response, REST_Controller::HTTP_OK);
+					}
 
+				}else{
 
-//				$this->mmodel->insert('doctor', $doctor_data);
-
-
-				// $this->mmodel->insert('doctor', $doctor_data);
-				// select the inserted record,
-				// instantiate consultant as follows
-				// $this->load->entity("EntityConsultant", , 'consultant');
-				// $this->response($this->consultant, REST_Controller::HTTP_OK);
-
-
-//				$response['status'] = REST_Controller::HTTP_OK;
-//				$response['doctor_id'] = $doctor_id;
-//				$response['msg'] = 'New Doctor Added Successfully';
-
-
-				$validation_result = $this->mdoctor->is_valid($post);
-
-				if ($validation_result['validation_status'] == 0) {
 					$response['status'] = REST_Controller::HTTP_BAD_REQUEST;
-					$response['msg'] = $validation_result['validation_msg'];
+					$response['msg'] = $this->mdoctor->validation_result[0];
 					$this->response($response, REST_Controller::HTTP_BAD_REQUEST);
-//					exit();
-				} else {
-					$response['status'] = REST_Controller::HTTP_OK;
-//					$response['doctor_id'] = $doctor_id;
-					$response['msg'] = 'New Doctor Added Successfully';
-					$this->response($response, REST_Controller::HTTP_OK);
 				}
 
 			} else {
 				$response['status'] = REST_Controller::HTTP_UNAUTHORIZED;
 				$this->response($response, REST_Controller::HTTP_UNAUTHORIZED);
-
 			}
+
 		} else {
 			$response['status'] = REST_Controller::HTTP_METHOD_NOT_ALLOWED;
 			$this->response($response, REST_Controller::HTTP_METHOD_NOT_ALLOWED);
--- application/models/Mdoctor.php
+++ application/models/Mdoctor.php
@@ -3,6 +3,10 @@ if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 class Mdoctor extends CI_Model
 {
+	public $validation_errors = array();
+	private $post = array();
+	protected $table = "doctor";
+
 	function __construct()
 	{
 		parent::__construct();
@@ -10,38 +14,89 @@ class Mdoctor extends CI_Model
 	}
 
 
-	public function is_valid($post_array)
-	{
-		$validation_errors = array();
+	public function set_data($post_array){
 
-		if (!(isset($post_array['salutation']) &&  $post_array['salutation'] != NULL && $post_array['salutation'] != '')) {
-			array_push($validation_errors, 'Invalid Salutation.');
-		}
-		if (!(isset($post_array['firstname']) &&  $post_array['firstname'] != NULL && $post_array['firstname'] != '')) {
-			array_push($validation_errors, 'Invalid First Name.');
-		}
-		if (!(isset($post_array['lastname']) &&  $post_array['lastname'] != NULL && $post_array['lastname'] != '')) {
-			array_push($validation_errors, 'Invalid Last Name.');
-		}
-		if (!(isset($post_array['slmc_reg_number']) &&  $post_array['slmc_reg_number'] != NULL && $post_array['slmc_reg_number'] != '')) {
-			array_push($validation_errors, 'Invalid SLMC Reg Number.');
-		}
-		if (!(isset($post_array['email']) &&  $this->mvalidation->email($post_array['email']))) {
-			array_push($validation_errors, 'Invalid Email.');
-		}
-		if (!(isset($post_array['device_mobile']) &&  $this->mvalidation->telephone($post_array['device_mobile']))) {
-			array_push($validation_errors, 'Invalid Device Mobile.');
-		}
-
-		if(sizeof($validation_errors)>0){
-			$result['validation_status'] = 0;
-		} else{
-			$result['validation_status'] = 1;
-		}
-		$result['validation_msg'] = $validation_errors;
-		return $result;
+		
+		$this->post['salutation'] = $post_array['salutation'];
+		$this->post['first_name'] = $post_array['firstname'];
+		$this->post['last_name'] = $post_array['lastname'];
+		$this->post['known_name'] = $post_array['wellknownas'];
+		$this->post['contact_telephone'] = $post_array['telephone'];
+		$this->post['email'] = $post_array['email'];
+		$this->post['specialities'] = $post_array['specialities'];
+		$this->post['slmc_reg_number'] = $post_array['slmc_reg_number'];
+		$this->post['consulting_hospitals'] = $post_array['consulting_hospitals'];
 
 	}
 
+	public function is_valid()
+	{
+		$result = true;
+
+		if (!(isset($this->post['salutation']) &&  $this->post['salutation'] != NULL && $this->post['salutation'] != '')){
+			array_push($this->validation_errors, 'Invalid Salutation.');
+			$result = false;
+		}
+
+		if (!(isset($this->post['firstname']) &&  $this->post['firstname'] != NULL && $this->post['firstname'] != '')) {
+			array_push($this->validation_errors, 'Invalid First Name.');
+			$result = false;
+		}
+
+		if (!(isset($this->post['lastname']) &&  $this->post['lastname'] != NULL && $this->post['lastname'] != '')) {
+			array_push($this->validation_errors, 'Invalid Last Name.');
+			$result = false;
+		}
+
+		if (!(isset($this->post['slmc_reg_number']) &&  $this->post['slmc_reg_number'] != NULL && $this->post['slmc_reg_number'] != '')) {
+			array_push($this->validation_errors, 'Invalid SLMC Reg Number.');
+			$result = false;
+		}
+
+		if (!(isset($this->post['email']) &&  $this->mvalidation->email($this->post['email']))) {
+			array_push($this->validation_errors, 'Invalid Email.');
+			$result = false;
+		}
+
+		if (!(isset($this->post['device_mobile']) &&  $this->mvalidation->telephone($this->post['device_mobile']))) {
+			array_push($this->validation_errors, 'Invalid Device Mobile.');
+			$result = false;
+		}
+
+		return $result;
+	}
+
+
+	/*
+	*
+	*/
+	public function create(){
+
+		$result = null;
+
+		$this->post['id'] = trim(com_create_guid(), '{}');
+		$this->post['is_deleted'] = 0;
+		$this->post['is_active'] = 1;
+		$this->post['updated'] = date("Y-m-d h:i:s");
+		$this->post['created'] = date("Y-m-d h:i:s");
+		$this->post['updated_by'] = $doctor_id;
+		$this->post['created_by'] = $doctor_id;
+
+		if( $this->insert($this->table, $this->post)){
+			$result = $this->get_doctor();
+		}
+
+		return $result;
+	}
+
+	/*
+	*
+	*/
+	public function get_doctor($id){
+		$CI = &get_instance();
+		$query_result = $this->select("*")->from($this->table)->where( sprintf("id='%s'", $id) )->getRow(0);
+		$CI->load->entity('EntityConsultant', $query_result, 'doctor_response');
+		return $CI->doctor_response;
+	}
 
 }
