c27f1f3ff15d738535607d26375f7b74fe1949b7
Buddhimal <bnawgunasekara@gmail.com>
2020-06-13 16:43:08 +0530

    Bug fix

--- application/entities/EntityClinicPendingPaymentDetails.php
+++ application/entities/EntityClinicPendingPaymentDetails.php
@@ -1,21 +1,48 @@
-<?php 
+<?php //
+//defined('BASEPATH') OR exit('No direct script access allowed');
+//
+//class EntityClinicPendingPaymentDetails{
+//
+//	public $sessions;
+//	public $grand_total;
+//	public $from;
+//	public $to;
+//
+//	public EntityClinicPendingPaymentDetails(){
+//		//default constructor
+//		$this->sessions = array();
+//		$this->grand_total = 0;
+//	}
+//
+//	public add_session($session){
+//		$this->sessions[] = $session;
+//		$this->grand_total += $session->total;
+//	}
+//}
+
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class EntityClinicPendingPaymentDetails{
+class EntityClinicPendingPaymentDetails
+{
 
-	public $sessions;
-	public $grand_total;
-	public $from;
-	public $to;
+    public $sessions;
+    public $grand_total;
+    public $from;
+    public $to;
 
-	public EntityClinicPendingPaymentDetails(){
-		//default constructor
-		$this->sessions = array();
-		$this->grand_total = 0;
-	}
 
-	public add_session($session){
-		$this->sessions[] = $session;
-		$this->grand_total += $session->total;
-	}
+    function __construct()
+    {
+        //default constructor
+        $this->sessions = array();
+        $this->grand_total = 0;
+    }
+
+
+    public function add_session($session)
+    {
+        $this->sessions[] = $session;
+        $this->grand_total += $session->total;
+    }
 }
