e7f6df57a8c8594dbc6a488f48bf6648bb3f0faa
Buddhimal <bnawgunasekara@gmail.com>
2020-06-13 20:12:01 +0530

    Changes

--- application/models/Mclinicholidays.php
+++ application/models/Mclinicholidays.php
@@ -1,73 +1,73 @@
-<?php 
+<?php
 if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 
 class Mclinicholidays extends CI_Model
 {
-	public $validation_errors = array();
-	private $post = array();
-	protected $table = "clinic_holidays";
+    public $validation_errors = array();
+    private $post = array();
+    protected $table = "clinic_holidays";
 
-	function __construct()
-	{
-		parent::__construct();
-		$this->load->model('mvalidation');
-	}
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('mvalidation');
+    }
 
 
-	public function set_data($post_array)
-	{
-		if (isset($post_array['holiday']))
-			$this->post['holiday'] = $post_array['holiday'];
-		if (isset($post_array['title']))
-			$this->post['title'] = $post_array['title'];
-		if (isset($post_array['note']))
-			$this->post['note'] = $post_array['note'];
-	}
+    public function set_data($post_array)
+    {
+        if (isset($post_array['holiday']))
+            $this->post['holiday'] = $post_array['holiday'];
+        if (isset($post_array['title']))
+            $this->post['title'] = $post_array['title'];
+        if (isset($post_array['note']))
+            $this->post['note'] = $post_array['note'];
+    }
 
-	public function is_valid()
-	{
-		unset($this->validation_errors);
-		$this->validation_errors = array();
+    public function is_valid()
+    {
+        unset($this->validation_errors);
+        $this->validation_errors = array();
 
-		$result = true;
+        $result = true;
 
-		if (!(isset($this->post['holiday']) && $this->post['holiday'] != NULL && $this->post['holiday'] != '' && $this->mvalidation->valid_date($this->post['holiday']) == TRUE)) {
-			array_push($this->validation_errors, 'Invalid Date format.');
-			$result = false;
-		}
+        if (!(isset($this->post['holiday']) && $this->post['holiday'] != NULL && $this->post['holiday'] != '' && $this->mvalidation->valid_date($this->post['holiday']) == TRUE)) {
+            array_push($this->validation_errors, 'Invalid Date format.');
+            $result = false;
+        }
 
-		return $result;
-	}
+        return $result;
+    }
 
-	/*
-	*
-	*/
-	public function create($clinic_id)
-	{
-		$result = null;
+    /*
+    *
+    */
+    public function create($clinic_id)
+    {
+        $result = null;
 
-		$session_id = trim($this->mmodel->getGUID(), '{}');
+        $session_id = trim($this->mmodel->getGUID(), '{}');
 
-		$this->post['id'] = $session_id;
-		$this->post['clinic_id'] = $clinic_id;
-		$this->post['is_deleted'] = 0;
-		$this->post['is_active'] = 1;
-		$this->post['updated'] = date("Y-m-d H:i:s");
-		$this->post['created'] = date("Y-m-d H:i:s");
-		$this->post['updated_by'] = $session_id;
-		$this->post['created_by'] = $session_id;
+        $this->post['id'] = $session_id;
+        $this->post['clinic_id'] = $clinic_id;
+        $this->post['is_deleted'] = 0;
+        $this->post['is_active'] = 1;
+        $this->post['updated'] = date("Y-m-d H:i:s");
+        $this->post['created'] = date("Y-m-d H:i:s");
+        $this->post['updated_by'] = $session_id;
+        $this->post['created_by'] = $session_id;
 
-		$this->mmodel->insert($this->table, $this->post);
+        $this->mmodel->insert($this->table, $this->post);
 
-		if ($this->db->affected_rows() > 0) {
-			$result = $this->get($session_id);
-		}
+        if ($this->db->affected_rows() > 0) {
+            $result = $this->get($session_id);
+        }
 
-		return $result;
-	}
+        return $result;
+    }
 
-    public function delete($clinic_id,$holiday_id)
+    public function delete($clinic_id, $holiday_id)
     {
         $this->db
             ->set('is_active', 0)
@@ -82,50 +82,51 @@ class Mclinicholidays extends CI_Model
     }
 
 
-	public function get($id)
-	{
+    public function get($id)
+    {
 
-		$query_result = $this->get_record($id);
+        $query_result = $this->get_record($id);
 
-		return $query_result;
-	}
+        return $query_result;
+    }
 
-	private function get_record($id)
-	{
+    private function get_record($id)
+    {
 
-		$this->db->select('id,holiday,title,note');
-		$this->db->from($this->table);
-		$this->db->where('id', $id);
-		return $this->db->get()->row();
-	}
+        $this->db->select('id,holiday,title,note');
+        $this->db->from($this->table);
+        $this->db->where('id', $id);
+        return $this->db->get()->row();
+    }
 
-	public function get_holidays($clinic_id, $year=null){
+    public function get_holidays($clinic_id, $year = null)
+    {
 
-		$output = array();
-		$date = $year;
-		if($date == null){
-			$date = date("Y");
-		}
+        $output = array();
+        $date = $year;
+        if ($date == null) {
+            $date = date("Y");
+        }
 
-		$start_date = date("Y-m-d", strtotime( sprintf("%s-01-01", $date )));
-		$end_date = date("Y-m-d", strtotime( sprintf("%s-12-31", $date )));
+        $start_date = date("Y-m-d", strtotime(sprintf("%s-01-01", $date)));
+        $end_date = date("Y-m-d", strtotime(sprintf("%s-12-31", $date)));
 
-		$all_holidays= $this->db
-            ->select( array('id', 'holiday','title','note'))
+        $all_holidays = $this->db
+            ->select(array('id', 'holiday', 'title', 'note'))
             ->from($this->table)
-            ->where( sprintf("holiday > '%s' and holiday < '%s' and clinic_id ='%s'", $start_date, $end_date, $clinic_id) )
-            ->where('is_active',1)
-            ->where('is_deleted',0)
+            ->where(sprintf("holiday > '%s' and holiday < '%s' and clinic_id ='%s'", $start_date, $end_date, $clinic_id))
+            ->where('is_active', 1)
+            ->where('is_deleted', 0)
             ->get();
 
-		foreach($all_holidays->result() as $holiday_data) {
-			$holiday['id'] = $holiday_data->id;
-			$holiday['holiday'] = $holiday_data->holiday;
-			$output[] = $holiday;
-		}
-		return $output;
+        foreach ($all_holidays->result() as $holiday_data) {
+            $holiday['id'] = $holiday_data->id;
+            $holiday['holiday'] = $holiday_data->holiday;
+            $output[] = $holiday;
+        }
+        return $output;
 
-	}
+    }
 
     public function valid_holiday($id)
     {
