7937301ec88bbd6c660c6fcf2695e29b36418f13
Buddhimal <bnawgunasekara@gmail.com>
2020-06-24 23:50:50 +0530

    bug fix

--- application/models/Mclinicsession.php
+++ application/models/Mclinicsession.php
@@ -204,7 +204,7 @@ class Mclinicsession extends CI_Model
             $sessions = new EntityClinicSession($session_data);
             $sessions->days = $this->mclinicsessiondays->get_today_session($sessions->id, $day);
             $sessions->days->appointment_count = $this->mclinicappointment->get_appointment_count_for_today($sessions->id);
-            $sessions->days->on_the_way = ($this->mlinicsessiontrans->check_session_already_updated($sessions->id, SessionStatus::ON_THE_WAY));
+            $sessions->days->on_the_way = ($this->mclinicsessiontrans->check_session_already_updated($sessions->id, SessionStatus::ON_THE_WAY));
             $sessions->consultant = $this->mdoctor->get($sessions->consultant);
             $output[] = $sessions;
         }
