b92087953285113b932d36f8fcb711c04871d167
Buddhimal <bnawgunasekara@gmail.com>
2020-08-03 01:26:02 +0530

    Change sms add is clinic to get appointment where

--- application/helpers/sms_helper.php
+++ application/helpers/sms_helper.php
@@ -17,22 +17,22 @@ class SMSTemplate{
 
     public static function OnTheWaySMS($data)
     {
-        return 'Ayubowan ' . $data['patient_name'] . '! Dr. '.$data['doctor_name'] .' is on his way to the clinic '.$data['clinic_name'].' at '.$data['clinic_city'].'.';
+        return '' . $data['patient_name'] . ', Dr. '.$data['doctor_name'] .' is on his way to the clinic '.$data['clinic_name'].' at '.$data['clinic_city'].'.';
     }
 
     public static function CancelSessionSMS($data)
     {
         $myDateTime = DateTime::createFromFormat('Y-m-d', $data['appointment_date']);
-        $appointment_date = $myDateTime->format('d F Y');
+        $appointment_date = $myDateTime->format('d F');
         $myDateTime = DateTime::createFromFormat('H:i:s', $data['starting_time']);
         $appointment_time = $myDateTime->format('h:i A');
 
-        return 'Ayubowan ' . $data['patient_name'] . '! Your appointment No.'.$data['serial_number'].' with Dr. '.$data['doctor_name'] .' at Clinic '.$data['clinic_name'].' '.$data['clinic_city'].', on '.$appointment_date.' '.$appointment_time.' is canceled due to an inevitable reason.';
+        return '' . $data['patient_name'] . ', Your appointment No.'.$data['serial_number'].' with Dr. '.$data['doctor_name'] .' at Clinic '.$data['clinic_name'].' '.$data['clinic_city'].', on '.$appointment_date.' '.$appointment_time.' is canceled due to an inevitable reason.';
     }
 
     public static function StartSessionSMS($data)
     {
-        return 'Ayubowan ' . $data['patient_name'] . '! Dr. '.$data['doctor_name'] .' has arrived at clinic '.$data['clinic_name'].' at '.$data['clinic_city'].'.';
+        return '' . $data['patient_name'] . ', Dr. '.$data['doctor_name'] .' has arrived at clinic '.$data['clinic_name'].' at '.$data['clinic_city'].'.';
     }
 }
 
